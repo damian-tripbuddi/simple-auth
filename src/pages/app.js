8f6afc986672578dd1da11af918012877d40d9d1
damian-helloworld <damian.sutton@helloworld.com.au>
2019-09-20 00:03:11 +1000

    Trying some extra pages.

--- src/pages/app.js
+++ src/pages/app.js
@@ -1,21 +1,26 @@
-import React from "react"
-import { Router } from "@reach/router"
-import Layout from "../components/Layout"
-import Details from "../components/Details"
-import Home from "../components/Home"
-import Login from "../components/Login"
-import PrivateRoute from "../components/PrivateRoute"
-import Status from "../components/Status"
+import React from 'react';
+import { Router } from '@reach/router';
+import Layout from '../components/Layout';
+import Details from '../components/Details';
+import Home from '../components/Home';
+import Login from '../components/Login';
+import PrivateRoute from '../components/PrivateRoute';
+import Status from '../components/Status';
+
+const Test = () => {
+  return <div>Test From Dynamic</div>;
+};
 
 const App = () => (
   <Layout>
     <Status />
     <Router>
-      <PrivateRoute path="/app/profile" component={Home} />
-      <PrivateRoute path="/app/details" component={Details} />
-      <Login path="/app/login" />
+      <Test path='/app/test' />
+      <PrivateRoute path='/app/profile' component={Home} />
+      <PrivateRoute path='/app/details' component={Details} />
+      <Login path='/app/login' />
     </Router>
   </Layout>
-)
+);
 
-export default App
+export default App;
